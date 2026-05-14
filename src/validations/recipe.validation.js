00d1a7e090f569db3788b64b31e4740e0ae79244
yara-e <yaraelsayed237@gmail.com>
2026-05-14 19:14:11 +0300

    feat: implement Joi validation

--- src/validations/recipe.validation.js
+++ src/validations/recipe.validation.js
@@ -3,9 +3,8 @@ import Joi from "joi";
 export const addRecipeSchema = Joi.object({
   title: Joi.string().min(3).max(100).required(),
   description: Joi.string().min(10).required(),
-  ingredients: Joi.array().items(Joi.string()).min(1).required(),
-  instructions: Joi.string().required(),
   categoryId: Joi.string().hex().length(24).required(),
+  createdBy: Joi.string().hex().length(24).required(),
   image: Joi.string(),
 });
 
@@ -13,9 +12,8 @@ export const updateRecipeSchema = Joi.object({
   id: Joi.string().hex().length(24).required(),
   title: Joi.string().min(3).max(100),
   description: Joi.string().min(10),
-  ingredients: Joi.array().items(Joi.string()),
-  instructions: Joi.string(),
-  categoryId: Joi.string().hex().length(24).required(),
+  categoryId: Joi.string().hex().length(24),
+  createdBy: Joi.string().hex().length(24),
 });
 
 export const deleteRecipeSchema = Joi.object({
