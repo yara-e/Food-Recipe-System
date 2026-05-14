4bc672d03fce3cd5b4c6b53b3ef6c8993bfc741a
yara-e <yaraelsayed237@gmail.com>
2026-05-14 19:27:36 +0300

    feat: implement Joi validation

--- src/modules/recipe/recipe.routes.js
+++ src/modules/recipe/recipe.routes.js
@@ -7,18 +7,24 @@ import {
   deleteRecipe,
 } from "./recipe.controller.js";
 import { upload } from "../../utils/upload.js";
+import { validate } from "../../middlewares/validation.middleware.js";
+import {
+  addRecipeSchema,
+  deleteRecipeSchema,
+  updateRecipeSchema,
+} from "./../../validations/recipe.validation";
 
 const recipeRouter = Router();
 
 recipeRouter
   .route("/")
-  .post(upload , addRecipe)
+  .post(upload, validate(addRecipeSchema), addRecipe)
   .get(getAllRecipes);
 
 recipeRouter
   .route("/:id")
   .get(getOneRecipe)
-  .put(upload , updateRecipe)
-  .delete(deleteRecipe);
+  .put(upload, validate(updateRecipeSchema), updateRecipe)
+  .delete(validate(deleteRecipeSchema), deleteRecipe);
 
 export { recipeRouter };
